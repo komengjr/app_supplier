ef834bbdb2029a6cf231d94e86a9911442c20067
komengjr <47206605+komengjr@users.noreply.github.com>
2026-03-11 09:51:08 +0700

    updtadttd

--- resources/views/application/laporan/report/daftar-supplier-rujukan-terpilih.blade.php
+++ resources/views/application/laporan/report/daftar-supplier-rujukan-terpilih.blade.php
@@ -322,35 +322,39 @@ $nocabang = 'XX';
                 </td>
             </tr>
         </table>
-        @if ($team->count() >= 5)
-        <table style="width: 100%; padding-top: 50px; font-size: 10px;" border="0">
-            @else
-            <table style="width: 100%; padding-top: 50px;" border="0">
-                @endif
-                <tr>
-                    <td>Team Evaluasi</td>
-                </tr>
-                <tr>
-                    @foreach ($team as $teams)
-                    <td>
-                        {{ $teams->log_master_team_jabatan }}
-                        <br><br><br><br><br>
-                        {{ $teams->log_master_team_name }}
-                        <br>
-                        {{ $teams->log_master_team_nip }}
-                    </td>
-                    @endforeach
-                </tr>
-            </table>
-            {{-- <div id="thanks">Thank you!</div> --}}
-            <div id="notices">
+        <table width="100%" border="0" cellspacing="0" cellpadding="5">
+            <tr>
+                <td>Team Evaluasi</td>
+            </tr>
+            <tr>
+                @foreach($team as $key => $teams)
 
-                <!-- <img style="padding-top: 1px; left: 10px;"
+                <td width="25%">
+                    {{ $teams->log_master_team_jabatan }}
+                    <br><br><br><br><br>
+                    {{ $teams->log_master_team_name }}
+                    <br>
+                    {{ $teams->log_master_team_nip }}
+                </td>
+
+                @if(($key+1)%4==0)
+            </tr>
+            <tr>
+                @endif
+
+                @endforeach
+            </tr>
+
+        </table>
+        {{-- <div id="thanks">Thank you!</div> --}}
+        <div id="notices">
+
+            <!-- <img style="padding-top: 1px; left: 10px;"
                 src="data:image/png;base64, {!! base64_encode(QrCode::style('round')->format('svg')->size(70)->errorCorrection('H')->generate(123)) !!}"> -->
 
 
-                <div class="notice">Dokumen Tidak Bisa di Ubah</div>
-            </div>
+            <div class="notice">Dokumen Tidak Bisa di Ubah</div>
+        </div>
     </main>
 </body>
 
